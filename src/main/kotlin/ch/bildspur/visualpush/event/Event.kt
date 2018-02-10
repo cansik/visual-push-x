6053360f344a655df261283f90c950b4e5a20c85
Florian Bruggisser <florian@nexpose.ch>
2018-02-10 16:05:39 +0100

    added remove and add handlers

--- src/main/kotlin/ch/bildspur/visualpush/event/Event.kt
+++ src/main/kotlin/ch/bildspur/visualpush/event/Event.kt
@@ -6,8 +6,24 @@ package ch.bildspur.visualpush.event
 class Event<T> {
     private val handlers = arrayListOf<(Event<T>.(T) -> Unit)>()
 
-    operator fun plusAssign(handler: Event<T>.(T) -> Unit) {
+    fun add(handler: Event<T>.(T) -> Unit) : Event<T>.(T) -> Unit
+    {
         handlers.add(handler)
+        return handler
+    }
+
+    fun remove(handler: Event<T>.(T) -> Unit) : Boolean
+    {
+        return handlers.remove(handler)
+    }
+
+    operator fun plusAssign(handler: Event<T>.(T) -> Unit) {
+        add(handler)
+    }
+
+    operator fun minusAssign(handler: Event<T>.(T) -> Unit)
+    {
+        remove(handler)
     }
 
     operator fun invoke(value: T) {
